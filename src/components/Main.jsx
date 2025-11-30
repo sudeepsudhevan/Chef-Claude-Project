7c96fe6a22800d234a884e6da6974a8a9af28b6c
Sudeep Sudhevan <31392327+sudeepsudhevan@users.noreply.github.com>
2025-11-30 13:15:37 +0530

    Added ingredient state and added data using form with React state Variable

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,16 +1,39 @@
-import React from 'react'
+import { useState } from "react";
 
 function Main() {
+  const [ingredients, setIngredients] = useState([])
+
+  const listElement = ingredients.map((ingredient) => {
+    return <li key={ingredient}>{ingredient}</li>
+  })
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget)
+    const newIngredient = formData.get("ingredient")
+
+    setIngredients(
+      prevIngredients => [
+        ...prevIngredients,
+        newIngredient
+      ]
+    )
+  }
+
   return (
     <main className='main'>
-        <form className='add-ingredient-form'>
-            <input 
-                aria-label='Add ingredient' 
-                type="text" 
-                placeholder='e.g. oregano'
-            />
-            <button>Add ingredient</button>
-        </form>
+      <form className='add-ingredient-form' onSubmit={handleSubmit}>
+        <input
+          aria-label='Add ingredient'
+          type="text"
+          placeholder='e.g. oregano'
+          name="ingredient"
+        />
+        <button>Add ingredient</button>
+      </form>
+      <ul>
+        {listElement}
+      </ul>
     </main>
   )
 }
