82f5e573b3636904f0ed32f8b6ecb1e2c28153c3
Sudeep Sudhevan <31392327+sudeepsudhevan@users.noreply.github.com>
2025-12-02 16:58:17 +0530

    Added a section for ingredient and for get a recipe container and styled

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 function Main() {
   const [ingredients, setIngredients] = useState([])
 
-  const listElement = ingredients.map((ingredient) => {
+  const ingredientsListItems = ingredients.map((ingredient) => {
     return <li key={ingredient}>{ingredient}</li>
   })
 
@@ -29,9 +29,19 @@ function Main() {
         />
         <button>Add ingredient</button>
       </form>
-      <ul>
-        {listElement}
-      </ul>
+      <section>
+        <h2>Ingredients on hand:</h2>
+        <ul className="ingredients-list" aria-live="polite">
+          {ingredientsListItems}
+        </ul>
+        <div className="get-recipe-container">
+          <div>
+            <h3>Ready for a recipe?</h3>
+            <p>Generate a recipe from your list of ingredients.</p>
+          </div>
+          <button>Get a recipe</button>
+        </div>
+      </section>
     </main>
   )
 }
