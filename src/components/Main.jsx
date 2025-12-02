58b5f3ad3dd82ce82e82d2eb63c5db853969ee6f
Sudeep Sudhevan <31392327+sudeepsudhevan@users.noreply.github.com>
2025-12-02 17:19:20 +0530

    Added conditional rendering in the new section of ingredient list

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -29,19 +29,23 @@ function Main() {
         />
         <button>Add ingredient</button>
       </form>
-      <section>
-        <h2>Ingredients on hand:</h2>
-        <ul className="ingredients-list" aria-live="polite">
-          {ingredientsListItems}
-        </ul>
-        <div className="get-recipe-container">
-          <div>
-            <h3>Ready for a recipe?</h3>
-            <p>Generate a recipe from your list of ingredients.</p>
-          </div>
-          <button>Get a recipe</button>
-        </div>
-      </section>
+      {ingredients.length > 0 && (
+        <section>
+          <h2>Ingredients on hand:</h2>
+          <ul className="ingredients-list" aria-live="polite">
+            {ingredientsListItems}
+          </ul>
+          {ingredients.length > 3 && (
+            <div className="get-recipe-container">
+              <div>
+                <h3>Ready for a recipe?</h3>
+                <p>Generate a recipe from your list of ingredients.</p>
+              </div>
+              <button>Get a recipe</button>
+            </div>
+          )}
+        </section>
+      )}
     </main>
   )
 }
