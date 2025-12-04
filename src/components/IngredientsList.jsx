0bf1f6243d8922c8fbb3d5b37d0b4007c84cdbb2
Sudeep Sudhevan <31392327+sudeepsudhevan@users.noreply.github.com>
2025-12-04 07:54:31 +0530

    Refactor to seperate components

---
+++ src/components/IngredientsList.jsx
@@ -0,0 +1,28 @@
+
+
+function IngredientsList({ingredients, toggleRecipeShown}) {
+
+    const ingredientsListItems = ingredients.map((ingredient) => {
+        return <li key={ingredient}>{ingredient}</li>
+    })
+
+    return (
+        <section>
+            <h2>Ingredients on hand:</h2>
+            <ul className="ingredients-list" aria-live="polite">
+                {ingredientsListItems}
+            </ul>
+            {ingredients.length > 3 && (
+                <div className="get-recipe-container">
+                    <div>
+                        <h3>Ready for a recipe?</h3>
+                        <p>Generate a recipe from your list of ingredients.</p>
+                    </div>
+                    <button onClick={toggleRecipeShown}>Get a recipe</button>
+                </div>
+            )}
+        </section>
+    )
+}
+
+export default IngredientsList
